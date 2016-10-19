5ff207cc73dc5dada9bd47827b90d8cc520def27
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2016-10-19 17:59:57 +0200

    [Missions] Create a ground control and share MOOSVars
    Create a ground moos file to control the boat by wifi
    Create pShare configurations for enable YODA exchange info with ground control.

---
+++ missions/boat/guerledan/ground.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source "../../../scripts/source.sh"
+nsplug ground.moos targ_ground.moos --path=../common:../../../data/maps -f
+pAntler targ_ground.moos >& /dev/null &
+
+printf "Mission started.\n"
+
+ANSWER="0"
+while [ "${ANSWER}" != "q" ]; do
+  printf "Hit (q) to Exit and Kill:\n"
+  printf "> "
+  read ANSWER
+done
+
+enstabretagnekill >& /dev/null &
+printf "Done killing processes...\n"
+rm targ_*
